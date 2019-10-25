b4520a0b52c29c244c6166c360337e8357c52d2a
ZhongYY96 <1219466560@qq.com>
2019-10-25 12:26:29 +0800

    slightly modified

--- server.py
+++ server.py
@@ -6,4 +6,4 @@ print('The server is ready to receive')
 while True:
     message, clientAddress = serverSocket.recvfrom(2048)
     modifiedMessage=message.decode().upper()
-    serverSocket.sendto(modifiedMessage.encode,clientAddress)
+    serverSocket.sendto(modifiedMessage.encode,clientAddressgit)
