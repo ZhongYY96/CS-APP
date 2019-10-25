1b4a511822fd0fe9d4b13c0763c78422305397a6
ZhongYY96 <1219466560@qq.com>
2019-10-25 10:20:16 +0800

    add two files client.py,server.py

---
+++ client.py
@@ -0,0 +1,9 @@
+from socket import *
+serverName = '10.102.150.35'
+serverPort = 12000
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+message = raw_input('Input lowercase sentence:')
+clientSocket.sendto(message.encode(), (serverName, serverPort))
+modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+print(modifiedMessage.decode())
+clientSocket.close()
