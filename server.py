d06d3687fd23cc9332a3faec33cde2bf342cddaf
ZhongYY96 <1219466560@qq.com>
2019-10-25 12:10:49 +0800

    server.py changed

--- server.py
+++ server.py
@@ -1,12 +1,9 @@
 from socket import *
 serverPort = 12000
-serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(('', serverPort))
-serverSocket.listen(1)
 print('The server is ready to receive')
 while True:
-    connectionSocket, addr = serverSocket.accept()
-    sentence = connectionSocket.recv(1024).decode()
-    capitalizedSentence = sentence.upper()
-    connectionSocket.send(capitalizedSentence.encode())
-    connectionSocket.close()
+    message, clientAddress = serverSocket.recvfrom(2048)
+    modifiedMessage=message.decode().upper()
+    serverSocket.sendto(modifiedMessage.encode,clientAddress)
